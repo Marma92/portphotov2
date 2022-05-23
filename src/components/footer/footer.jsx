b0608eedaad7a29250e487c34ad9276f9dee2b0a
Amram ELBAZ <elbaz.amram@gmail.com>
2022-05-23 23:29:23 +0200

    [new] footer makeover, I should stop

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,8 @@
 import { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { instagram, twitter, github, darkroom, email } from '../../constants/user.constants'
-import { faTwitter, faInstagram, faGithubAlt } from '@fortawesome/free-brands-svg-icons'
-import { faAt, faFilm } from '@fortawesome/free-solid-svg-icons';
+import { faInstagramSquare, faTwitterSquare, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelopeSquare, faFilm } from '@fortawesome/free-solid-svg-icons';
 import "./footer.css";
 
 
@@ -12,11 +12,11 @@ export class Footer extends Component {
     return (
       <div style={styles.footer} className="footer">
       <p>Developped with ❤️ under <a href="https://opensource.org/licenses/MIT"> MIT Licence</a>, 2017-{new Date().getFullYear()}, <a href="http://amramelbaz.com">ELBAZ Amram</a></p>
-      <a href={instagram} target="_blank" rel="noreferrer" style={styles.icons}><FontAwesomeIcon icon={faInstagram}  color="#ffc106" size="3x"/></a>
-      <a href={darkroom}  target="_blank" rel="noreferrer" style={styles.icons}><FontAwesomeIcon icon={faFilm} color="#be4d25" size="3x"/> </a>
-      <a href={twitter} target="_blank" rel="noreferrer" style={styles.icons}><FontAwesomeIcon icon={faTwitter}  color="#4099FF" size="3x"/></a>
-      <a href={'mailto:'+ email} target="_blank" rel="noreferrer" style={styles.icons}><FontAwesomeIcon icon={faAt} color="#25a5be" size="3x"/></a>
-      <a href={github} target="_blank" rel="noreferrer" style={styles.icons}><FontAwesomeIcon icon={faGithubAlt}  color="#ac41f4" size="3x"/></a>
+      <a href={instagram} target="_blank" rel="noreferrer" style={styles.icons} ><FontAwesomeIcon icon={faInstagramSquare} size="2x"/></a>
+      <a href={darkroom}  target="_blank" rel="noreferrer" style={styles.icons} className='dr'><FontAwesomeIcon icon={faFilm} size="2x"/> </a>
+      <a href={twitter} target="_blank" rel="noreferrer" style={styles.icons} className='tw'><FontAwesomeIcon icon={faTwitterSquare} size="2x"/></a>
+      <a href={'mailto:'+ email} target="_blank" rel="noreferrer" style={styles.icons} className='em'><FontAwesomeIcon icon={faEnvelopeSquare} size="2x"/></a>
+      <a href={github} target="_blank" rel="noreferrer" style={styles.icons} className='gh'><FontAwesomeIcon icon={faGithubSquare} size="2x"/></a>
       </div>
     )
   }
@@ -29,7 +29,7 @@ const styles = {
     borderTop: '1px solid #d0d7de',
   },
   icons :{
-    margin : '1%',
+    margin : '0.5%',
     paddingBottom : '1%',
   }
 }
